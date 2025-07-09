d044aafcdf897355d94509647bdfbdb39761bdba
Jack210797 <suvorov21079@gmail.com>
2025-07-09 16:20:56 +0300

    recruiter bag fixed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,23 +9,50 @@ const Navbar = () => {
   const { user } = useUser()
   const navigate = useNavigate()
 
-  const { setShowRecruiterLogin } = useContext(AppContext)
+  const { setShowRecruiterLogin, companyToken, companyData } = useContext(AppContext)
+
   return (
     <div className="shadow py-4">
       <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
         <img onClick={() => navigate('/')} className="cursor-pointer" src={assets.logo} alt="" />
         {user ? (
           <div className="flex items-center gap-3">
+            {companyToken && companyData && (
+              <div className="flex items-center gap-3">
+                <Link className="relative group" to={'/dashboard'}>
+                  <img className="w-8 border rounded-full" src={companyData.image} alt={companyData.name} />
+                </Link>
+                <p>|</p>
+              </div>
+            )}
             <Link to={'/applications'}>Applied Jobs</Link>
             <p>|</p>
             <p className="max-sm:hidden ">Hi, {user.firstName + (user.lastName ? ' ' + user.lastName : '')}</p>
             <UserButton />
           </div>
         ) : (
-          <div className="flex gap-4" max-sm:text-xs>
-            <button onClick={(e) => setShowRecruiterLogin(true)} className="text-gray-600">
-              Recruiter Liogin
-            </button>
+          <div className="flex gap-4 max-sm:text-xs">
+            {companyToken && companyData ? (
+              <div className="flex items-center gap-3">
+                <Link className="w-7 h-7 relative flex rounded-full" to={'/dashboard'}>
+                  <img className="w-full h-full object-cover" src={companyData.image} alt={companyData.name} />
+                </Link>
+                <p>|</p>
+              </div>
+            ) : (
+              <button
+                onClick={(e) => {
+                  if (companyToken) {
+                    navigate('/dashboard')
+                  } else {
+                    setShowRecruiterLogin(true)
+                  }
+                }}
+                className="text-gray-600"
+              >
+                Recruiter Login
+              </button>
+            )}
             <button
               onClick={(e) => {
                 openSignIn()
